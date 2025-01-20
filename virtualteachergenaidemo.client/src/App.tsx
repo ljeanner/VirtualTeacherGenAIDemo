7ffa8f8d9b2c2a2856e5dde39926c6b47b23f55c
Lucile Jeanneret <lujeanne@microsoft.com>
2025-01-20 23:28:11 +0100

    fix

--- virtualteachergenaidemo.client/src/App.tsx
+++ virtualteachergenaidemo.client/src/App.tsx
@@ -15,7 +15,7 @@ import { InteractionType } from '@azure/msal-browser';
 import UserDisplay from './auth/userDisplay';
 import { UserProvider } from './auth/UserContext';
 import { UserRoleProvider, useUserRole } from './auth/UserRoleContext';
-import { getUserRole, createUser } from './services/UserService';
+import { getUserRole, createUser } from './services/userService';
 import { LocalizationProvider, useLocalization } from './contexts/LocalizationContext';
 
 function App(props: any) {
